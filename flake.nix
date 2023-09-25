811c8110940524eb9019164f05d175fd28a08b94
sgonzalez <sgonzalezoyuela@gmail.com>
2023-09-25 01:16:18 -0300

    first commit

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "Typescript/HTML neovim";
+  inputs = {
+    neovim-flake.url = "github:jordanisaacs/neovim-flake";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    neovim-flake,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+        inherit (nixpkgs) lib;
+        baseNeovim = neovim-flake.packages.${system}.nix;
+
+        # Typescript
+        tsCfg = {
+          #config.vim.theme.name = "dracula-nvim";
+          config.vim.languages.nix.enable = false;
+          config.vim.languages.ts.enable = true;
+          config.vim.languages.html.enable = true;
+          config.vim.languages.sql.enable = true;
+        };
+
+        ts.neovim-ts = baseNeovim.extendConfiguration {
+          modules = [tsCfg];
+          inherit pkgs;
+        };
+
+        # Golang
+        goCfg = {
+          #config.vim.theme.name = "dracula-nvim";
+          config.vim.languages.nix.enable = false;
+          config.vim.languages.go.enable = true;
+        };
+
+        go.neovim-go = baseNeovim.extendConfiguration {
+          modules = [goCfg];
+          inherit pkgs;
+        };
+      in {
+        packages = rec {
+          inherit (ts) neovim-ts;
+          inherit (go) neovim-go;
+          default = neovim-ts;
+        };
+      }
+    );
+}
